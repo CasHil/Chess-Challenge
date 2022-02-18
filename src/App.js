f441583a3f6bdd3b33a821757e008a660a863544
Casper <casperhildebrand@gmail.com>
2022-02-18 17:29:49 +0100

    Create initial chessboard

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import ChallengeChessBoard from "./components/ChallengeChessBoard";
+
+function App() {
+  return (
+    <div className="App">
+      <ChallengeChessBoard />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ChallengeChessBoard.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import Chess from "chess.js";
+import { Chessboard } from "react-chessboard";
+
+export default function PlayRandomMoveEngine() {
+  const [game, setGame] = useState(new Chess());
+
+  function safeGameMutate(modify) {
+    setGame((g) => {
+      const update = { ...g };
+      modify(update);
+      return update;
+    });
+  }
+
+  function onDrop(sourceSquare, targetSquare) {
+    let move = null;
+    safeGameMutate((game) => {
+      move = game.move({
+        from: sourceSquare,
+        to: targetSquare,
+      });
+    });
+    if (move === null) return false;
+    return true;
+  }
+
+  return <Chessboard position={game.fen()} onPieceDrop={onDrop} />;
+}
